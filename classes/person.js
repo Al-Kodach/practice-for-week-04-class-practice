c5c9aa997c504e1c77510d37d3944bffac2eb4c8
Al-kodach <alkodach351@gmail.com>
2023-04-09 13:29:49 -0400

    completed

--- classes/person.js
+++ classes/person.js
@@ -1,4 +1,35 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  // introduce instance method
+
+  introduce() {
+    console.log(
+      `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`
+    );
+  }
+
+  // class method that introduce all person
+  static introducePeople(people) {
+    if (!Array.isArray(people)) {
+      throw new Error(
+        "introducePeople only takes an array as an argument."
+      );
+    }
+
+    if (!people.every((per) => per instanceof Person)) {
+      throw new Error(
+        "All items in array must be Person class instances."
+      );
+    }
+
+    people.forEach((per) => per.introduce());
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
